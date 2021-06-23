a501dee03698cb503573db69f58d7a9b23466f04
Cindy Mit <cindymit89@gmail.com>
2021-06-23 11:28:43 -0400

    Updated song and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :songs
+  has_many :comments, through: :songs
 
   validates :password, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
