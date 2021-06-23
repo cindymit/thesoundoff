d66329a28ce043f63656785b78264d0893daf7b5
Cindy Mit <cindymit89@gmail.com>
2021-06-23 15:09:06 -0400

    Updated user model and authentication controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :songs
-  has_many :comments, through: :songs
+  has_many :comments
 
   validates :password, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true
