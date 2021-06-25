af19fce9bd2f60287f6e0af2f22850b2cb3e67d8
Cindy Mit <cindymit89@gmail.com>
2021-06-25 07:50:33 -0400

    Added ternary statement for access to Layout for authorized users and unauthorized users

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,17 +1,34 @@
 import "./Layout.css";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export default function Layout(props) {
+  const { currentUser, handleLogout, children } = props;
+
   return (
-    <div className='layout'>
+    <div className="layout">
       <header>
-        <Link to='/'><h1>thesoundoff.</h1></Link>
-        <Link to='/login'>Login</Link>
-        <Link to='/signup'>Signup</Link>
+        <Link to="/">
+          <h1>thesoundoff.</h1>
+        </Link>
+        {currentUser ? (
+          <>
+            <p>Welcome back, {currentUser.username}! <button onClick={handleLogout}>Logout</button></p>
+            
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </>
+        )}
+        {currentUser && (
+          <>
+            <Link to="/songs">All Songs</Link>
+          </>
+        )}
         <hr />
       </header>
       {props.children}
     </div>
-  )
+  );
 }
