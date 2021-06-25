83c1eab08e12119be92cdfbfcbe8564a95687721
Cindy Mit <cindymit89@gmail.com>
2021-06-25 08:44:21 -0400

    Added switch and routes to main jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,59 @@
-import './App.css';
-import Login from './screens/Login/Login'
-import Signup from './screens/Signup/Signup'
-import Layout from './components/Layout/Layout'
-import Main from './containers/Main/Main'
-import { Switch, Route, useHistory } from 'react-router-dom'; 
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Login from "./screens/Login/Login";
+import Signup from "./screens/Signup/Signup";
+import Layout from "./components/Layout/Layout";
+import Main from "./containers/Main";
+import { Switch, Route, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 import {
   loginUser,
   signupUser,
   verifyUser,
   removeToken,
-} from './services/users';
+} from "./services/users";
 
 function App() {
-	const [currentUser, setCurrentUser] = useState(null);
-	const history = useHistory();
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
 
-	useEffect(() => {
-		const handleVerify = async () => {
-			const userData = await verifyUser();
-			setCurrentUser(userData);
-		};
-		handleVerify();
-	}, []);
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
 
-	const handleLogin = async (formData) => {
-		const userData = await loginUser(formData);
-		setCurrentUser(userData);
-		history.push('/');
-	};
+  const handleLogin = async (formData) => {
+    const userData = await loginUser(formData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
 
-	const handleSignup = async (formData) => {
-		const userData = await signupUser(formData);
-		setCurrentUser(userData);
-		history.push('/');
-	};
+  const handleSignup = async (formData) => {
+    const userData = await signupUser(formData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
 
-	const handleLogout = () => {
-		setCurrentUser(null);
-		localStorage.removeItem('authToken');
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem("authToken");
     removeToken();
-    history.push('/');
-	};
+    history.push("/");
+  };
 
   return (
     <div className="App">
       <Layout currentUser={currentUser} handleLogout={handleLogout}>
         <Switch>
-          <Route path='/login'>
+          <Route path="/login">
             <Login handleLogin={handleLogin} />
           </Route>
-          <Route path='/signup'>
+          <Route path="/signup">
             <Signup handleRegister={handleSignup} />
           </Route>
-          <Route path='/'>
+          <Route path="/">
             <Main />
           </Route>
         </Switch>
--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+
+import { Switch, Route } from 'react-router-dom';
 import "./Main.css";
 
 export default function Main() {
   return (
-    <div>
+    <div className="main">
       <h1>This is the main container!</h1>
+      <Switch>
+        <Route path='/songs/:id/edit'>
+          <h3>Edit Song</h3>
+        </Route>
+        <Route path='/songs/new'>
+          <h3>Create A Song</h3>
+        </Route>
+        <Route path='/songs'>
+          <h3>Delete Song</h3>
+        </Route>
+      </Switch>
     </div>
   )
 }
