5d1a0a257b01fd5a27e604e6aaeaaed4d205cedb
Cindy Mit <cindymit89@gmail.com>
2021-06-28 12:10:28 -0400

    Some layout, login, and home css and jsx modifications

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -5,27 +5,41 @@ export default function Layout(props) {
   const { currentUser, handleLogout, children } = props;
 
   return (
-    <div className="layout">
+    <div className="layout-container">
       <header>
+        <div className="logo-div">
         <Link to="/">
-          <h1>thesoundoff.</h1>
-        </Link>
+          <img
+            src="https://i.imgur.com/dN5rghq.png"
+            alt="logo"
+            className="logo"
+          />
+          </Link>
+        </div>
         {currentUser ? (
           <>
-            <p>Welcome back, {currentUser.username}! <button onClick={handleLogout}>Logout</button></p>
-            
+            <p>
+              <div className="welcome">
+              Welcome back, {currentUser.username}!{" "} <br/>
+                <button onClick={handleLogout}>Logout</button>
+              </div>
+            </p>
           </>
         ) : (
           <>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            <div className="register-links">
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
           </>
         )}
         {currentUser && (
           <>
-            <Link to="/songs">All Songs</Link>
-            <Link to="/songs/new">Create</Link>
-            <Link to="/comments">Comments</Link>
+            <div className="authorized-links">
+              <Link to="/">Home</Link><br/>
+              <Link to="/songs">All Songs</Link><br/>
+              <Link to="/songs/new">Create</Link>
+            </div>
           </>
         )}
       </header>
--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,11 +1,11 @@
-import './Login.css';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import "./Login.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Login(props) {
   const [formData, setFormData] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
 
   const { username, password } = formData;
@@ -20,28 +20,51 @@ export default function Login(props) {
   };
 
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      handleLogin(formData);
-    }}>
-        <h1>
-          LOGIN
-        </h1>
-        <label>
-          Username:
-          <input type='text' name='username' value={username} onChange={handleChange}/>
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type='password' name='password' value={password} onChange={handleChange}/>
-        </label>
-        <br />
-      <button>Submit</button>
-      <h4>Don't have an account?
-        <br />
-        <Link to='/signup'>Sign-up Here!</Link>
-      </h4>
-      </form>
+    <div className="signin-container">
+      <div
+        className="login-banner"
+        style={{
+          backgroundImage: "url(https://i.imgur.com/A6ax1A2.jpg)",
+        }}
+      >
+        {" "}
+      </div>
+      <div className="login-container">
+        <form className="login-form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLogin(formData);
+          }}
+        >
+          <h1>LOGIN</h1>
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <button>Submit</button>
+          <h4>
+            Don't have an account?
+            <br />
+            <Link to="/signup">Sign-up Here!</Link>
+          </h4>
+        </form>
+      </div>
+    </div>
   );
 }
