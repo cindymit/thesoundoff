ac978e7b9eaac4a001c44f81342b77648ed17f45
Cindy Mit <cindymit89@gmail.com>
2021-06-28 13:32:50 -0400

    CreateSong CSS and JSX modifications

--- client/src/screens/CreateSong/CreateSong.jsx
+++ client/src/screens/CreateSong/CreateSong.jsx
@@ -26,65 +26,69 @@ export default function CreateSong(props) {
 
   return (
     <div className="create-container">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleCreate(formData);
-        }}
-      >
-        <h1>Tell us about your favorite song here!</h1>
-        <input
-          type="text"
-          name="artist"
-          value={artist}
-          placeholder="Artist"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="title"
-          value={title}
-          placeholder="Title"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="genre"
-          value={genre}
-          placeholder="Genre"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="album"
-          value={album}
-          placeholder="Album"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="year_released"
-          value={year_released}
-          placeholder="Year Released"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="image_url"
-          value={image_url}
-          placeholder="Image URL"
-          onChange={handleChange}
-        />
-        <button
+      <div className="create-form">
+        <form
           onSubmit={(e) => {
             e.preventDefault();
             handleCreate(formData);
-            history.push("/songs");
           }}
         >
-          Submit
-        </button>
-      </form>
+          <h2>Tell us about your favorite song here!</h2>
+          <input
+            type="text"
+            name="artist"
+            value={artist}
+            placeholder="Artist"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="title"
+            value={title}
+            placeholder="Title"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="genre"
+            value={genre}
+            placeholder="Genre"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="album"
+            value={album}
+            placeholder="Album"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="year_released"
+            value={year_released}
+            placeholder="Year Released"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="image_url"
+            value={image_url}
+            placeholder="Image URL"
+            onChange={handleChange}
+          />
+          <div className="create-btn-div"></div>
+          <button
+            className="create-btn"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleCreate(formData);
+              history.push("/songs");
+            }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
